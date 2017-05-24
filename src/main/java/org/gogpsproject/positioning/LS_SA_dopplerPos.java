d7f645b52d7464cefdc1db359995c152cb78521d
ZiglioNZ <emanuele@ziglioli.org>
2017-05-24 14:13:45 +1200

    run sats.select for each iteration

--- src/main/java/org/gogpsproject/positioning/LS_SA_dopplerPos.java
+++ src/main/java/org/gogpsproject/positioning/LS_SA_dopplerPos.java
@@ -22,83 +22,83 @@ public class LS_SA_dopplerPos extends LS_SA_code {
 
     double max_iterations = 20; 
 
-    //    sats.selectSatellitesStandaloneFractional(obs, -100);
-    sats.selectStandalone(obs, -100);
-
-    // Number of available satellites (i.e. observations)
-    int nObsAvail = sats.avail.size();
-    if( nObsAvail < MINSV ){
-      if( goGPS.isDebug() ) System.out.println("dopplerPos, not enough satellites for " + obs.getRefTime() );
-      if( rover.status == Status.None ){
-        rover.status = Status.NotEnoughSats;
-      }
-      rover.setXYZ(0, 0, 0);
-      return;
-    }
-    
-    /** range rate */
-    double[] rodot = new double[nObsAvail];
-
-    /** Least squares design matrix */
-    SimpleMatrix A = new SimpleMatrix( nObsAvail, nUnknowns );
-
-    // Set up the least squares matrices
-    for (int i = 0; i < nObsAvail; i++) {
-
-      int satId = obs.getSatID(i);
-
-      if( sats.pos[i] == null  || !sats.avail.contains(satId) ) {//|| recpos.ecef==null || sats.pos[i].ecef==null ){
-        continue;
-      }
-
-      ObservationSet os = obs.getSatByID(satId);
-
-      // scalar product of speed vector X unit vector
-      float doppler = os.getDoppler(ObservationSet.L1);
-
-      ////
-      // Line Of Sight vector units (ECEF)
-      SimpleMatrix e = new SimpleMatrix(1,3);
-      e.set( 0,0, rover.diffSat[i].get(0) / rover.satAppRange[i] );
-      e.set( 0,1, rover.diffSat[i].get(1) / rover.satAppRange[i] );
-      e.set( 0,2, rover.diffSat[i].get(2) / rover.satAppRange[i] );
-      double rodotSatSpeed   = -e.mult( sats.pos[i].getSpeed() ).get(0);
-      double dopplerSatSpeed = -rodotSatSpeed*Constants.FL1/Constants.SPEED_OF_LIGHT;
-      ///
-
-      if( Float.isNaN( doppler )){
-        // Line Of Sight vector units (ECEF)
-        rodot[i] = rodotSatSpeed;
-      }
-      else {
-        // scalar product of speed vector X unit vector
-        rodot[i] = doppler * Constants.SPEED_OF_LIGHT/Constants.FL1;
-        
-//        if( Math.abs(doppler - dopplerSatSpeed)>200)
-//          System.out.println("diff here");
-//        rodot[i] = rodotSatSpeed;
-        os.getDoppler(ObservationSet.L1);
-        System.out.println( String.format( "%2d) snr:%2.0f doppler:%6.0f; satSpeed:%6.0f; D:%6.0f", 
-            satId,
-            os.getSignalStrength(ObservationSet.L1),
-            doppler, 
-            dopplerSatSpeed,
-            doppler - dopplerSatSpeed ));
-      }
-      
-      // build A matrix
-      A.set(i, 0, sats.pos[i].getSpeed().get(0) ); /* X */
-      A.set(i, 1, sats.pos[i].getSpeed().get(1) ); /* Y */
-      A.set(i, 2, sats.pos[i].getSpeed().get(2) ); /* Z */
-      
-      double satpos_norm = Math.sqrt(Math.pow(sats.pos[i].getX(), 2)
-                                   + Math.pow(sats.pos[i].getY(), 2)
-                                   + Math.pow(sats.pos[i].getZ(), 2));
-      A.set(i, 3, satpos_norm ); 
-    }
-    
     for (int itr = 0; itr < max_iterations; itr++) {
 
+      //    sats.selectSatellitesStandaloneFractional(obs, -100);
+      sats.selectStandalone(obs, -100);
+
+      // Number of available satellites (i.e. observations)
+      int nObsAvail = sats.avail.size();
+      if( nObsAvail < MINSV ){
+        if( goGPS.isDebug() ) System.out.println("dopplerPos, not enough satellites for " + obs.getRefTime() );
+        if( rover.status == Status.None ){
+          rover.status = Status.NotEnoughSats;
+        }
+        rover.setXYZ(0, 0, 0);
+        return;
+      }
+      
+      /** range rate */
+      double[] rodot = new double[nObsAvail];
+
+      /** Least squares design matrix */
+      SimpleMatrix A = new SimpleMatrix( nObsAvail, nUnknowns );
+
+      // Set up the least squares matrices
+      for (int i = 0; i < nObsAvail; i++) {
+
+        int satId = obs.getSatID(i);
+
+        if( sats.pos[i] == null  || !sats.avail.contains(satId) ) {//|| recpos.ecef==null || sats.pos[i].ecef==null ){
+          continue;
+        }
+
+        ObservationSet os = obs.getSatByID(satId);
+
+        // scalar product of speed vector X unit vector
+        float doppler = os.getDoppler(ObservationSet.L1);
+
+        ////
+        // Line Of Sight vector units (ECEF)
+        SimpleMatrix e = new SimpleMatrix(1,3);
+        e.set( 0,0, rover.diffSat[i].get(0) / rover.satAppRange[i] );
+        e.set( 0,1, rover.diffSat[i].get(1) / rover.satAppRange[i] );
+        e.set( 0,2, rover.diffSat[i].get(2) / rover.satAppRange[i] );
+        double rodotSatSpeed   = -e.mult( sats.pos[i].getSpeed() ).get(0);
+        double dopplerSatSpeed = -rodotSatSpeed*Constants.FL1/Constants.SPEED_OF_LIGHT;
+        ///
+
+        if( Float.isNaN( doppler )){
+          // Line Of Sight vector units (ECEF)
+          rodot[i] = rodotSatSpeed;
+        }
+        else {
+          // scalar product of speed vector X unit vector
+          rodot[i] = doppler * Constants.SPEED_OF_LIGHT/Constants.FL1;
+          
+//          if( Math.abs(doppler - dopplerSatSpeed)>200)
+//            System.out.println("diff here");
+//          rodot[i] = rodotSatSpeed;
+          os.getDoppler(ObservationSet.L1);
+          System.out.println( String.format( "%2d) snr:%2.0f doppler:%6.0f; satSpeed:%6.0f; D:%6.0f", 
+              satId,
+              os.getSignalStrength(ObservationSet.L1),
+              doppler, 
+              dopplerSatSpeed,
+              doppler - dopplerSatSpeed ));
+        }
+        
+        // build A matrix
+        A.set(i, 0, sats.pos[i].getSpeed().get(0) ); /* X */
+        A.set(i, 1, sats.pos[i].getSpeed().get(1) ); /* Y */
+        A.set(i, 2, sats.pos[i].getSpeed().get(2) ); /* Z */
+        
+        double satpos_norm = Math.sqrt(Math.pow(sats.pos[i].getX(), 2)
+                                     + Math.pow(sats.pos[i].getY(), 2)
+                                     + Math.pow(sats.pos[i].getZ(), 2));
+        A.set(i, 3, satpos_norm ); 
+      }
+      
       SimpleMatrix b = new SimpleMatrix( nObsAvail, 1 );
 
       double pivotSNR = 0;

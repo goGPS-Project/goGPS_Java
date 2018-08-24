71ee152d809c28f62527967c40aa85b31c16205b
Emanuele Ziglioli <emanuele@fppnt.com>
2018-08-24 14:33:53 +0100

    long index

--- src/main/java/org/gogpsproject/producer/parser/ublox/UBXFileReader.java
+++ src/main/java/org/gogpsproject/producer/parser/ublox/UBXFileReader.java
@@ -81,8 +81,8 @@ public class UBXFileReader extends EphemerisSystem implements ObservationsProduc
 	@Override
 	public Coordinates getDefinedPosition() {
 		Coordinates coord = Coordinates.globalXYZInstance(0.0, 0.0, 0.0); //new Coordinates(new SimpleMatrix(3, 1));
-		//coord.setXYZ(0.0, 0.0, 0.0 );
-		coord.computeGeodetic();
+		coord.setXYZ(0.0, 0.0, 0.0 );
+//		coord.computeGeodetic();
 		// TODO should return null?
 		return coord;
 	}
